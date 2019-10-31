0366b6ac3f81d26baee7778b7a8cc4bdc7c23a69
= <=>
2019-10-31 16:34:47 -0600

    inicializacion de proyecto

---
+++ Hola.java
@@ -0,0 +1,8 @@
+public class Hola {
+	public static void main(String[] args){
+	System.out.print("Hola cohort 17");
+	System.out.print("Todo bien");
+	}
+}
+// Un cambio el que sea
+
---
+++ otra.java
@@ -0,0 +1,5 @@
+public class otra {
+	public  static void main (String[] args){
+	System.out.println("otra clase");
+}
+}
